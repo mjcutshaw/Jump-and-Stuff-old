ade6062180dec1359e049aea28dd2d1f4914d28a
Matt <mjcutshaw@gmail.com>
2022-08-25 19:26:51 -0400

    pause and options menu

--- UI/PlayButton.gd
+++ UI/PlayButton.gd
@@ -2,6 +2,7 @@ extends Button
 
 func _ready() -> void:
 	grab_focus()
+	connect("pressed", self, "_on_Play_pressed")
 
 func _on_Play_pressed() -> void:
 	get_tree().change_scene("res://Levels/Playground/Playground.tscn")
--- UI/Quit.gd
+++ UI/Quit.gd
@@ -1,6 +1,7 @@
 extends Button
 
-
+func _ready() -> void:
+	connect("pressed", self, "_on_Quit_pressed")
 
 func _on_Quit_pressed() -> void:
 	get_tree().quit()
