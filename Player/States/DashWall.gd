d16750413851aa29a3de716aaa68d475b8412729
Matt <mjcutshaw@gmail.com>
2022-09-06 00:05:21 -0400

    more dashes

---
+++ Player/States/DashWall.gd
@@ -0,0 +1,45 @@
+extends DashState
+
+
+func enter() -> void:
+	.enter()
+
+	
+
+
+func exit() -> void:
+	.exit()
+
+	
+
+
+func physics(delta) -> void:
+	.physics(delta)
+
+	
+
+
+func visual(delta) -> void:
+	.visual(delta)
+
+	
+
+
+func handle_input(event: InputEvent) -> int:
+	var newState = .handle_input(event)
+	if newState:
+		return newState
+
+	
+
+	return State.Null
+
+
+func state_check(delta: float) -> int:
+	var newState = .state_check(delta)
+	if newState:
+		return newState
+
+	
+
+	return State.Null
