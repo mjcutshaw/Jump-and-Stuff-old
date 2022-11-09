006956c474c80c27f3927594283eaf7c26061ac1
Matt <mjcutshaw@gmail.com>
2022-11-08 19:51:49 -0500

    created more settings menus

--- UI/Menu/Scripts/SettingsMenu.gd
+++ UI/Menu/Scripts/SettingsMenu.gd
@@ -3,6 +3,9 @@ extends BaseMenu
 onready var settingsContainer: MarginContainer = $M/H/Options/M
 onready var gameplayMenu: VBoxContainer = $"%GameplayMenu"
 onready var accessibilityMenu: VBoxContainer = $"%AccessibilityMenu"
+onready var videoMenu: VBoxContainer = $"%VideoMenu"
+onready var audioMenu: VBoxContainer = $"%AudioMenu"
+onready var keybindingMenu: VBoxContainer = $"%KeybindgsMenu"
 onready var backButton: Button = $"%Back"
 onready var gameplayButton: Button = $"%Gameplay"
 onready var accessibilityButton: Button = $"%Accessibility"
@@ -15,6 +18,9 @@ func _ready() -> void:
 	backButton.connect("pressed", self, "back_button_pressed")
 	gameplayButton.connect("pressed", self, "show_gameplay")
 	accessibilityButton.connect("pressed", self, "show_accessibility")
+	videoButton.connect("pressed", self, "show_video")
+	audioButton.connect("pressed", self, "show_audio")
+	keybindingsButton.connect("pressed", self, "show_keybindings")
 
 func enter() -> void:
 	set_paused(true)
@@ -51,4 +57,16 @@ func show_gameplay() -> void:
 func show_accessibility() -> void:
 	menu_hid()
 	accessibilityMenu.visible = true
+
+func show_video() -> void:
+	menu_hid()
+	videoMenu.visible = true
+
+func show_audio() -> void:
+	menu_hid()
+	audioMenu.visible = true
+
+func show_keybindings() -> void:
+	menu_hid()
+	keybindingMenu.visible = true
 
